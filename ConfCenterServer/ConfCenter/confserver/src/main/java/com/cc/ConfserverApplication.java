e751caecd6c93e2893f742cf696c37d5190f7184
神牛步行3 <841202396@qq.com>
2018-08-30 08:51:55 +0800

    add 统一配置中心server项目初始化

---
+++ ConfCenterServer/ConfCenter/confserver/src/main/java/com/cc/ConfserverApplication.java
@@ -0,0 +1,12 @@
+package com.cc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfserverApplication.class, args);
+	}
+}
